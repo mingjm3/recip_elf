de4c4d3009746fbde05b3b4f2851443fd00867a6
Arcgo <imingjun.ma@outlook.com>
2023-07-08 00:40:54 -0700

    [feat] implement /recipegen api

--- api/app.js
+++ api/app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var recipegenRouter = require('./routes/recipegen');
 
 var app = express();
 
@@ -19,8 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/recipegen', recipegenRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ api/routes/recipegen.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+const { OpenAIApi } = require("openai");
+
+router.post('/', function(req, res, next) {
+  var requestBody = req.body;
+  var users = requestBody.users;
+  var cuisines = requestBody.cuisines;
+  var dietaryRestrictions = requestBody.dietaryRestrictions;
+  var foods = requestBody.foods;
+
+  var messages = [
+    { role: "system", content: "You are a helpful assistant." },
+    { role: "user", content: "Hello!" },
+  ];
+
+  var userMessage = {
+    role: "user",
+    content: `I'm looking for recipes based on the following criteria:
+      Cuisines: ${cuisines.join(", ")}
+      Dietary Restrictions: ${dietaryRestrictions.join(", ")}
+      Foods: ${foods.join(", ")}`
+  };
+  messages.push(userMessage);
+
+  console.log(messages);
+
+  axios.post('https://api.openai.com/v1/chat/completions', {
+    model: "gpt-3.5-turbo",
+    messages: messages
+  }, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer API Key'
+    }
+  })
+  .then(function(response) {
+    var generatedText = response.data.choices[0].message.content;
+    res.json({ response: generatedText });
+  })
+  .catch(function(error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server Error' });
+  });
+});
+
+module.exports = router;
